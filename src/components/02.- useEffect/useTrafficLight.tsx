e9e5fd8c3eb1d619feab6f1953080238b3a38da4
CrissVargasUAGRM <cristhianingsis@gmail.com>
2025-11-03 22:58:42 -0400

    useReduce

--- src/components/02.- useEffect/useTrafficLight.tsx
+++ src/components/02.- useEffect/useTrafficLight.tsx
@@ -8,16 +8,16 @@ const colors = {
     green: 'bg-green-500 animate-pulse'
 };
 
-export const TrafficLightWithUseEffect = () => {
+export const UseTrafficLight = () => {
 
     const [light, setLight] = useState<LightColor>('red');
 
     const [countdown, setCountdown] = useState(5);
 
     useEffect(() => {
-        if(countdown === 0){
-
-        }
+      if(countdown === 0){
+        return;
+      }
 
       const intervalId = setInterval(() => {
         setCountdown((prev) => prev - 1);
@@ -49,19 +49,15 @@ export const TrafficLightWithUseEffect = () => {
       };
     }, [countdown, light])
     
-    
-
     type LightColor = keyof typeof colors;
 
-    return (
-        <div className="min-h-screen bg-gradient-to-br from-slate-900 via-gray-900 to-slate-800 flex items-center justify-center p-4">
-        <div className="flex flex-col items-center space-y-8">
-            <h1 className="text-white text-2xl">Semaforo con useEffect</h1>
-            <h2 className="text-white text-xl">Countdown: {countdown}</h2>
-            <div className={`w-32 h-32 ${light === 'red' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
-            <div className={`w-32 h-32 ${light === 'yellow' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
-            <div className={`w-32 h-32 ${light === 'green' ? colors[light] : 'bg-gray-500'} rounded-full`}></div>
-        </div>
-        </div>
-    );
+    return {
+      countdown,
+      light,
+      colors,
+      percentaje: (countdown / 5) * 100,
+      greenLight: light === 'green' ? colors[light] : 'bg-gray-500',
+      redLight: light === 'red' ? colors[light] : 'bg-gray-500',
+      yellowLight: light === 'yellow' ? colors[light] : 'bg-gray-500',
+    };
 };
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { HooksApp } from './components/HooksApp.tsx'
 import { TrafficLight } from './components/01.- useState/TrafficLight.tsx'
-import { TrafficLightWithUseEffect } from './components/02.- useEffect/TrafficLightWithEffect.tsx'
+import { TrafficLightWithHook } from './components/hooks/TrafficLightWithHook.tsx'
+import { TasksApp } from './components/03.- useReducer/TasksApp.tsx'
+//import { TrafficLightWithUseEffect } from './components/02.- useEffect/useTrafficLight.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <HooksApp /> */}
-    <TrafficLightWithUseEffect />
+    {/* <TrafficLightWithHook /> */}
+    <TasksApp/>
   </StrictMode>,
 )
